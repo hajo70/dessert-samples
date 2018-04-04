3cc651295de47b9284131a0696bab317bec616a6
anderscore-hjhessmann <33529783+anderscore-hjhessmann@users.noreply.github.com>
2018-04-04 22:22:52 +0200

    New sample shows using groups

--- src/test/java/de/spricom/dessert/test/samples/DessertGroupSampleTest.java
+++ src/test/java/de/spricom/dessert/test/samples/DessertGroupSampleTest.java
@@ -2,20 +2,28 @@ package de.spricom.dessert.test.samples;
 
 import de.spricom.dessert.assertions.SliceAssertions;
 import de.spricom.dessert.groups.PackageSlice;
+import de.spricom.dessert.groups.SliceGroup;
 import de.spricom.dessert.slicing.Slice;
 import de.spricom.dessert.slicing.SliceContext;
-import de.spricom.dessert.groups.SliceGroup;
 import org.junit.Test;
 
 import java.io.IOException;
 
-public class DependencyCheck {
+public class DessertGroupSampleTest {
+
+    @Test
+    public void testCycleFree() throws IOException {
+        Slice slice = new SliceContext().packageTreeOf("de.spricom.dessert");
+        SliceGroup<PackageSlice> subPackages = SliceGroup.splitByPackage(slice);
+        SliceAssertions.dessert(slice).splitByPackage().isCycleFree();
+    }
 
     @Test
     public void testPackageDependencies() throws IOException {
         Slice slice = new SliceContext().packageTreeOf("de.spricom.dessert");
         SliceGroup<PackageSlice> subPackages = SliceGroup.splitByPackage(slice);
-        SliceAssertions.dessert(slice).splitByPackage().isCycleFree();
-        subPackages.forEach(pckg -> SliceAssertions.assertThat(pckg).doesNotUse(pckg.getParentPackage(subPackages)));
+
+        subPackages.forEach(pckg -> SliceAssertions.assertThat(pckg)
+                .doesNotUse(pckg.getParentPackage(subPackages)));
     }
 }
