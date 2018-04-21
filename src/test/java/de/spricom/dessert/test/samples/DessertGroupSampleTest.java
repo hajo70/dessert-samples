62ae803466f1a4790f8c13b89a1ebc01a9e2f9b0
anderscore-hjhessmann <33529783+anderscore-hjhessmann@users.noreply.github.com>
2018-04-21 09:15:21 +0200

    Adds nesting rule and duplicates tests.

--- src/test/java/de/spricom/dessert/test/samples/DessertGroupSampleTest.java
+++ src/test/java/de/spricom/dessert/test/samples/DessertGroupSampleTest.java
@@ -3,16 +3,18 @@ package de.spricom.dessert.test.samples;
 import de.spricom.dessert.assertions.SliceAssertions;
 import de.spricom.dessert.groups.PackageSlice;
 import de.spricom.dessert.groups.SliceGroup;
+import de.spricom.dessert.slicing.ConcreteSlice;
 import de.spricom.dessert.slicing.Slice;
 import de.spricom.dessert.slicing.SliceContext;
+import org.junit.Ignore;
 import org.junit.Test;
 
-import java.io.IOException;
+import static org.fest.assertions.Assertions.assertThat;
 
 public class DessertGroupSampleTest {
 
     @Test
-    public void testCycleFree() throws IOException {
+    public void testCycleFree() {
         Slice slice = new SliceContext().packageTreeOf("de.spricom.dessert");
 
         SliceGroup<PackageSlice> sg = SliceGroup.splitByPackage(slice);
@@ -23,12 +25,30 @@ public class DessertGroupSampleTest {
     }
 
     @Test
-    public void testPackageDependencies() throws IOException {
+    public void testNestingRuleNoParentPackage() {
         Slice slice = new SliceContext().packageTreeOf("de.spricom.dessert");
         SliceGroup<PackageSlice> packages = SliceGroup.splitByPackage(slice);
 
         packages.forEach(pckg -> SliceAssertions.assertThat(pckg)
                 .doesNotUse(pckg.getParentPackage(packages)));
 
+    }
+
+    @Test
+    public void testNestingRuleNoAncestorPackage() {
+        Slice slice = new SliceContext().packageTreeOf("de.spricom.dessert");
+        SliceGroup<PackageSlice> packages = SliceGroup.splitByPackage(slice);
+
+        packages.forEach(pckg -> SliceAssertions.assertThat(pckg)
+                .doesNotUse(slice.slice(entry -> pckg.getParentPackageName().startsWith(entry.getPackageName()))));
+    }
+
+    @Ignore
+    @Test
+    public void testNoDuplicates() {
+        ConcreteSlice duplicates = new SliceContext().duplicates();
+        StringBuilder sb = new StringBuilder();
+        duplicates.getSliceEntries().forEach(entry -> sb.append(entry.getURI()).append("\n"));
+        assertThat(duplicates.getSliceEntries()).as(sb.toString()).isEmpty();
     }
 }
